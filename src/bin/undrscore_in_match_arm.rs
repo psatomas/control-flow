84b690b5f58a321960b7f08c7d96e9d94f9ece90
psatomas <psatomas@gmail.com>
2025-04-16 18:06:50 -0300

    feat: add underscore_in_match_arm.rs

---
+++ src/bin/undrscore_in_match_arm.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let season = "autumn";
+
+    match season {
+        "summer" => println!("school's out"),
+        "winter" => println!("Brr, so cold!"),
+        _ => print!("lots of rain"),
+    }
+}
