c22d4ca0c3a5498e784de449b208014df5165131
psatomas <psatomas@gmail.com>
2025-04-16 09:44:26 -0300

    feat: add the_else_if_statement.rs

---
+++ src/bin/the_else_if_statement.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let season = "summer";
+
+    if season == "summer" {
+        println!("school's out!");
+    } else if season == "winter" {
+        println!("Brr, so cold");
+    } else if season == "fall" {
+        println!("Leaves falling");
+    } else if season == "spring" {
+        println!("Lots of rain!")
+    }
+}
