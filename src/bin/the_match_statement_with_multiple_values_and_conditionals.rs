c015240cf9c92db9db7c3e91a843a02c25156d3e
psatomas <psatomas@gmail.com>
2025-04-16 19:04:24 -0300

    feat: add the_match_statement_with_multiple_values_and_conditionals.rs

---
+++ src/bin/the_match_statement_with_multiple_values_and_conditionals.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let number = 5;
+    match number {
+        value if value % 2 == 0 => println!("{value} is an even number"),
+        x if x % 2 != 0 => println!("{x} is an odd number"),
+        _ => unreachable!(),
+    }
+}
