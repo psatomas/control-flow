64052942b433bd854bac375d6f9ea0cb12efd79c
psatomas <psatomas@gmail.com>
2025-04-17 15:42:57 -0300

    feat: add the_loop_and_break_keywords.rs

---
+++ src/bin/the_loop_and_break_keywords.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut seconds = 10;
+    
+    loop {
+        if seconds == 0 {
+            println!("Blastoff!");
+            break;
+        }
+        
+        println!("{seconds} seconds to blastoff...");
+        seconds -= 1;
+    }
+}
