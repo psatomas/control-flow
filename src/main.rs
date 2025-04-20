fd93bda6b779df644e2589a593f506bd6e1e212f
psatomas <psatomas@gmail.com>
2025-04-20 17:42:23 -0300

    feat: updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-fn countdown(seconds: i32) {
-    if seconds == 0 {
-        println!("Blastoff!")
-    } else {
-        println!("{seconds} seconds to blastoff..");
-        countdown(seconds - 1);  
-    }
-    
-}
-
 fn main() {
-    countdown(5);
+
 }
