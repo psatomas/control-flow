09687c6816c765ae236875420380abb0046ab3cc
psatomas <psatomas@gmail.com>
2025-04-21 13:44:34 -0300

    feat: updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-
+    fn countdown(seconds: i32) {
+        if seconds == 0 {
+            println!("Blastoff!")
+        } else {
+            println!("{seconds} seconds to blastoff..");
+            countdown(seconds - 1);  
+        }
+        
+    }
+    
+    fn main() {
+        countdown(5);
+    }
 }
