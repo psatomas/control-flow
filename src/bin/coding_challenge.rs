6fde5cd2c1f2fd578dbf9911b6339bd0dda128a3
psatomas <psatomas@gmail.com>
2025-04-23 18:11:59 -0300

    feat: updated coding_challenge.rs

--- src/bin/coding_challenge.rs
+++ src/bin/coding_challenge.rs
@@ -1,13 +1,30 @@
 fn color_to_number(color: &str) -> i32 {
-    if color == "red" {
-        1
-    } else if color == "green" {
-        2
-    } else if color == "blue" {
-        3
-    } else {
-        0
+    match color {
+        "red" => 1,
+        "green" => 2,
+        "blue" =>3,
+        _ => 0,
     }
+}
+fn factorial_iterative(number: i32) -> i32 {
+    let mut product = 1;
+    let mut count = number;
+
+    while count > 0 {
+        product *= count;
+        count -= 1;
+    }
+
+    product
+
+}
+
+fn factorial_recursive(number: i32) -> i32 {
+    if number == 1 {
+        return 1;  
+    }
+    
+    number * factorial_recursive(number - 1)
 }
 
 fn main() {
@@ -15,4 +32,7 @@ fn main() {
     println!("{}", color_to_number("green"));
     println!("{}", color_to_number("blue"));
     println!("{}", color_to_number("purple"));
+
+    println!("{}", factorial_iterative(4));
+    println!("{}", factorial_recursive(4));
 }
